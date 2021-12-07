964f122fb18af8877f9fd5b75dc84e487481ab22
felipefe20 <67080028+felipefe20@users.noreply.github.com>
2021-12-07 09:31:40 -0500

    Update Streamlit_YF.py

--- Streamlit_YF.py
+++ Streamlit_YF.py
@@ -9,6 +9,9 @@ st.write("""
 Shown are the stock closing price and volume of Google!
 """)
 
+
+
+
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 tickerSymbol = 'TSLA'
@@ -17,6 +20,12 @@ tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-11-30')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+
+if st.button('Click aqui para descargar ALG de ayer'):
+    st.write('Downloading yesterday calls')
+else:
+    st.write('Goodbye')
 
 st.area_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
