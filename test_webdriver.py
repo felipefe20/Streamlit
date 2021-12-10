8306f51de12496837ea818f27948692ba3490512
felipefe20 <67080028+felipefe20@users.noreply.github.com>
2021-12-10 10:26:39 -0500

    Update test_webdriver.py

--- test_webdriver.py
+++ test_webdriver.py
@@ -6,8 +6,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
 options = Options()
-options.add_argument("--headless")
+#options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
