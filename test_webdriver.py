86942c45b5b24ef5af34fe7fbc0477b1de26cf10
felipefe20 <67080028+felipefe20@users.noreply.github.com>
2021-12-10 10:36:12 -0500

    Update test_webdriver.py

--- test_webdriver.py
+++ test_webdriver.py
@@ -6,9 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.common.by import By
 
 
 options = Options()
