6f390648b1d623da8c91cf19a4786f7026f0eae8
felipefe20 <67080028+felipefe20@users.noreply.github.com>
2021-12-13 17:36:27 -0500

    Update test_webdriver.py

--- test_webdriver.py
+++ test_webdriver.py
@@ -9,15 +9,38 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-features=NetworkService")
-options.add_argument("--window-size=1920x1080")
-options.add_argument("--disable-features=VizDisplayCompositor")
+#options = Options()
+#options.add_argument("--headless")
+#options.add_argument("--no-sandbox")
+#options.add_argument("--disable-dev-shm-usage")
+#options.add_argument("--disable-gpu")
+#options.add_argument("--disable-features=NetworkService")
+#options.add_argument("--window-size=1920x1080")
+#options.add_argument("--disable-features=VizDisplayCompositor")
 
+#initial settings
+def setting_selenium_options(download_file_path:str)->webdriver.ChromeOptions:
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-extensions")
+    options.add_argument("--headless")
+    options.add_argument("--proxy-server='direct://'")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    options.add_argument(f'user-agent={user_agent}')
+    prefs = {
+        "download.default_directory":download_file_path
+        #"download.prompt_for_download": False,
+        #"download.directory_upgrade": True
+        }
+
+    options.add_experimental_option('prefs', prefs)
+    return options
 
 def delete_selenium_log():
     if os.path.exists('selenium.log'):
@@ -60,6 +83,34 @@ def Login():
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="ctl00_BaseContent_mslp_btnLogin"]'))).click()
 
     st.write("Login succesful")
+
+    
+#Date to fetch
+def set_date_to_fetch(date:str)->str:
+    """
+    Funcion para setear la fecha que haremos fetch, si el usuario pone de parametro date = ayer, se hara fetch de el dia anterior, de lo contrario
+    puede pasar una fecha cualquiera por parametro y sobre esa fecha se hara el fetch
+
+    Args:
+        date (str): ayer o la fecha que se desee fetchear la info
+
+    Returns:
+        str: la fecha en formato string %m/%d/%Y 
+    """
+
+    if date == 'ayer':
+        today_date = datetime.now().strftime('%A').lower()
+        if today_date == 'monday':
+            yesterday_date = datetime.now() - timedelta(3)    
+        else:
+            yesterday_date = datetime.now() - timedelta(1)
+    else:
+        yesterday_date = pd.to_datetime(date)
+    
+    yesterday_date_str = yesterday_date.strftime('%m/%d/%Y')
+
+    return yesterday_date_str    
+
     
 def download_metadata_day(driver,day):
     if day=="yesterday":
